b1987b0afc468739672efd174ee023ed0836c8fe
Julka <j.a.czapiewska@gmail.com>
2023-01-11 21:34:56 +0100

    11.01.2023 NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
+
+const [avatarClicked, setAvatarClicked] = useState<boolean>(false);
+console.log(avatarClicked);
+
   return (
     <div className="App">
+      <NavBar avatarClicked={avatarClicked} setAvatarClicked={setAvatarClicked} />
 
     </div>
   );
